29b41d63730a3ea6a54937558dc51a746669d0ac
Praveent0710 <98657684+Praveent0710@users.noreply.github.com>
2022-06-16 12:33:20 +0530

    Add files via upload

---
+++ o/contact.php
@@ -0,0 +1,6 @@
+<?php
+ echo "Contact Numbers:
+    983467890-98
+    298378023987"
+?>
+<?php 
---
+++ o/inc/banner.inc.php
@@ -0,0 +1,2 @@
+<h1><a href="index.php">Student Tutor<span></span></a></h1>
+<h3><a href="index.php"></a></h3>
